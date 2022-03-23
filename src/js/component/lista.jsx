a9797527408b63e2f7e2190ec665b4befff14e2a
BenjaminNassW <benjanass@gmail.com>
2022-03-23 00:04:49 +0000

    listo

--- src/js/component/lista.jsx
+++ src/js/component/lista.jsx
@@ -2,20 +2,58 @@ import React, { useState, useEffect } from "react";
 
 const List = () => {
 	const [lista, setLista] = useState([]);
-	const [tarea, setTarea] = useState("");
+	const [tarea, setTarea] = useState({ label: "", done: false });
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/benjanass")
+			.then(
+				(resp) => resp.json() // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			)
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+				setLista(data);
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
+	}, []);
 
 	return (
 		<div>
 			<input
-				value={tarea}
+				value={tarea?.label}
 				placeholder="ingrese tarea"
 				onChange={(e) => {
-					setTarea(e.target.value);
-				}}></input>
+					setTarea({ label: e.target.value, done: false });
+				}}
+			/>
 			<button
 				onClick={() => {
 					setLista([...lista, tarea]);
-					setTarea("");
+					const nuevaLista = [...lista, tarea];
+					console.log(lista);
+					fetch(
+						"https://assets.breatheco.de/apis/fake/todos/user/benjanass",
+						{
+							method: "PUT",
+							body: JSON.stringify(nuevaLista),
+							headers: {
+								"Content-Type": "application/json",
+							},
+						}
+					)
+						.then(
+							(resp) => resp.json() // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+						)
+						.then((data) => {
+							//here is were your code should start after the fetch finishes
+							console.log(data); //this will print on the console the exact object received from the server
+						})
+						.catch((error) => {
+							//error handling
+							console.log(error);
+						});
 				}}>
 				agregar tarea
 			</button>
@@ -23,7 +61,7 @@ const List = () => {
 				{lista.map((tarea, i) => {
 					return (
 						<div id="borrar" key={i} className="d-flex mydivouter">
-							<p key={i}>{tarea}</p>
+							<p key={i}>{tarea.label}</p>
 							<button
 								className="mybuttonoverlap"
 								id="equis"
@@ -34,6 +72,29 @@ const List = () => {
 										return index !== i;
 									}
 									setLista(result);
+
+									fetch(
+										"https://assets.breatheco.de/apis/fake/todos/user/benjanass",
+										{
+											method: "PUT",
+											body: JSON.stringify(result),
+											headers: {
+												"Content-Type":
+													"application/json",
+											},
+										}
+									)
+										.then(
+											(resp) => resp.json() // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+										)
+										.then((data) => {
+											//here is were your code should start after the fetch finishes
+											console.log(data); //this will print on the console the exact object received from the server
+										})
+										.catch((error) => {
+											//error handling
+											console.log(error);
+										});
 								}}>
 								X
 							</button>
